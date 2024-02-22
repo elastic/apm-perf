b52650a9c804746b3ff018ad4c5b6b1a0a18ee8f
Carson Ip <carson.ip@elastic.co>
2024-02-22 11:07:05 +0000

    Fix license headers

--- internal/telemetrygen/metrics/metrics_types.go
+++ internal/telemetrygen/metrics/metrics_types.go
@@ -1,19 +1,12 @@
-// Licensed to The OpenTelemetry Authors under one or more contributor
-// license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright
-// ownership. The OpenTelemetry Authors licenses this file to you under
-// the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+// This file is forked from https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/cmd/telemetrygen,
+// which is licensed under Apache-2 and Copyright The OpenTelemetry Authors.
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+// It modifies the original implementation to remove unnecessary files,
+// and to accept a configurable logger.
 
 package metrics
 
