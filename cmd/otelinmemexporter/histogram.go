36dd8a610a5070086f6a0e0acf15ab1674d52c5a
Eric Yap <eric.yap@elastic.co>
2025-02-18 18:12:56 +0800

    Add explicit buckets quantile function

--- cmd/otelinmemexporter/histogram.go
+++ cmd/otelinmemexporter/histogram.go
@@ -1,11 +1,96 @@
 package otelinmemexporter
 
-import "go.opentelemetry.io/collector/pdata/pmetric"
+import (
+	"math"
+	"slices"
 
-type bucket struct {
-	count      int
-	lowerBound float64 // exclusive
-	upperBound float64 // inclusive
+	"go.opentelemetry.io/collector/pdata/pmetric"
+)
+
+type explicitBucket struct {
+	UpperBound float64 // inclusive
+	Count      uint    // beware that this will be converted to float64 in calculations
+}
+
+// explicitBucketsQuantile calculates the quantile `q` based on the given buckets.
+// The buckets are assumed to be contiguous, e.g. `(-Inf, 5], (5, 10], (10, +Inf]`, but can be
+// unsorted since the function will sort the buckets based on upper bound prior to calculation.
+// The buckets must also have the highest bound of +Inf.
+//
+// The quantile value is interpolated assuming a linear distribution within a bucket.
+// However, if the quantile falls into the highest bucket i.e. `(x, +Inf]`, the lower bound of that
+// bucket (x) is returned . On the other hand, if the quantile falls into the lowest bucket
+// i.e. `(-Inf, y]`, the upper bound of that bucket (y) is returned.
+//
+// Here are some special cases:
+//   - If `q` == NaN, NaN is returned.
+//   - If `q` < 0, -Inf is returned.
+//   - If `q` > 1, +Inf is returned.
+//   - If `buckets` has fewer than 2 elements, NaN is returned.
+//   - If the highest bucket is not +Inf, NaN is returned.
+//   - If `buckets` has 0 observations, NaN is returned.
+func explicitBucketsQuantile(q float64, buckets []explicitBucket) float64 {
+	if math.IsNaN(q) {
+		return math.NaN()
+	}
+	if q < 0 {
+		return math.Inf(-1)
+	}
+	if q > 1 {
+		return math.Inf(+1)
+	}
+
+	slices.SortFunc(buckets, func(a, b explicitBucket) int {
+		// We don't expect the bucket boundary to be a NaN.
+		if a.UpperBound < b.UpperBound {
+			return -1
+		}
+		if a.UpperBound > b.UpperBound {
+			return +1
+		}
+		return 0
+	})
+
+	if len(buckets) < 2 {
+		return math.NaN()
+	}
+
+	// The highest bound must be +Inf, and the lowest bound must be -Inf.
+	if !math.IsInf(buckets[len(buckets)-1].UpperBound, +1) {
+		return math.NaN()
+	}
+
+	// Check if there are any observations.
+	var observations uint
+	for _, bucket := range buckets {
+		observations += bucket.Count
+	}
+	if observations == 0 {
+		return math.NaN()
+	}
+
+	// Find the bucket that the quantile falls into.
+	rank := q * float64(observations)
+	var countSoFar uint
+	bucketIdx := slices.IndexFunc(buckets, func(bucket explicitBucket) bool {
+		countSoFar += bucket.Count
+		// Compare using GTE instead of GT since upper bound is inclusive.
+		return float64(countSoFar) >= rank
+	})
+
+	if bucketIdx == len(buckets)-1 {
+		return buckets[len(buckets)-2].UpperBound
+	}
+	if bucketIdx == 0 {
+		return buckets[0].UpperBound
+	}
+
+	// Interpolate to get quantile in bucket.
+	bucketStart := buckets[bucketIdx-1].UpperBound
+	bucketEnd := buckets[bucketIdx].UpperBound
+	count := buckets[bucketIdx].Count
+	bucketQuantile := (rank - float64(countSoFar-count)) / float64(count)
+	return bucketStart + (bucketEnd-bucketStart)*bucketQuantile
 }
 
 func mergeHistogramDataPoint(from, to pmetric.HistogramDataPoint) {
--- cmd/otelinmemexporter/histogram_test.go
+++ cmd/otelinmemexporter/histogram_test.go
@@ -1,13 +1,181 @@
 package otelinmemexporter
 
 import (
+	"fmt"
+	"maps"
+	"math"
+	"slices"
+	"sort"
 	"testing"
 	"time"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/pmetrictest"
+	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 )
+
+func Test_explicitBucketsQuantile_SpecialCases(t *testing.T) {
+	simpleBuckets := []explicitBucket{
+		{UpperBound: 10, Count: 1},
+		{UpperBound: 20, Count: 2},
+		{UpperBound: 30, Count: 4},
+		{UpperBound: math.Inf(+1), Count: 3},
+	}
+
+	type args struct {
+		p       float64
+		buckets []explicitBucket
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "quantile NaN",
+			args: args{p: math.NaN(), buckets: simpleBuckets},
+			want: math.NaN(),
+		},
+		{
+			name: "quantile less than 0",
+			args: args{p: -10, buckets: simpleBuckets},
+			want: math.Inf(-1),
+		},
+		{
+			name: "quantile greater than 1",
+			args: args{p: 4, buckets: simpleBuckets},
+			want: math.Inf(+1),
+		},
+		{
+			name: "1 bucket only",
+			args: args{p: 0.5, buckets: []explicitBucket{{Count: 1, UpperBound: math.Inf(+1)}}},
+			want: math.NaN(),
+		},
+		{
+			name: "highest bucket not +Inf",
+			args: args{
+				p: 0.2,
+				buckets: []explicitBucket{
+					{Count: 1, UpperBound: 1},
+					{Count: 2, UpperBound: 2},
+				},
+			},
+			want: math.NaN(),
+		},
+		{
+			name: "zero observations",
+			args: args{
+				p: 0.2,
+				buckets: []explicitBucket{
+					{Count: 0, UpperBound: 5},
+					{Count: 0, UpperBound: 10},
+					{Count: 0, UpperBound: math.Inf(+1)},
+				},
+			},
+			want: math.NaN(),
+		},
+		{
+			name: "lowest bucket",
+			args: args{p: 0.1, buckets: simpleBuckets},
+			want: 10.0,
+		},
+		{
+			name: "highest bucket",
+			args: args{p: 0.9, buckets: simpleBuckets},
+			want: 30.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assertInDeltaWithInfAndNaN(
+				t,
+				tt.want,
+				explicitBucketsQuantile(tt.args.p, tt.args.buckets),
+				0,
+				"explicitBucketsQuantile()",
+			)
+		})
+	}
+}
+
+func Test_explicitBucketsQuantile(t *testing.T) {
+
+	tests := []struct {
+		name       string
+		buckets    []explicitBucket
+		wantValues map[float64]float64
+	}{
+		{
+			name: "promql example",
+			buckets: []explicitBucket{
+				{UpperBound: 0, Count: 0},
+				{UpperBound: 10, Count: 10},
+				{UpperBound: 15, Count: 5},
+				{UpperBound: 20, Count: 0},
+				{UpperBound: 30, Count: 0},
+				{UpperBound: math.Inf(1), Count: 0},
+			},
+			wantValues: map[float64]float64{
+				0.5:  7.5,
+				0.9:  13.5,
+				0.99: 14.85,
+				1.0:  15.0,
+			},
+		},
+		{
+			name: "real example",
+			buckets: []explicitBucket{
+				{UpperBound: 0, Count: 0},
+				{UpperBound: 5, Count: 1053},
+				{UpperBound: 10, Count: 19},
+				{UpperBound: 25, Count: 1},
+				{UpperBound: math.Inf(+1), Count: 0},
+			},
+			wantValues: map[float64]float64{
+				0.5:  2.54748338082,
+				0.9:  4.58547008547,
+				0.95: 4.84021842355,
+				0.99: 7.43947368421,
+				1.0:  25.0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		quantiles := slices.Collect(maps.Keys(tt.wantValues))
+		sort.Float64s(quantiles)
+		for _, q := range quantiles {
+			t.Run(fmt.Sprintf("%s %.2f", tt.name, q), func(t *testing.T) {
+				assertInDeltaWithInfAndNaN(
+					t,
+					tt.wantValues[q],
+					explicitBucketsQuantile(q, tt.buckets),
+					1e-10,
+					"explicitBucketsQuantile()",
+				)
+			})
+		}
+	}
+}
+
+func assertInDeltaWithInfAndNaN(t *testing.T, want, got, delta float64, msgAndArgs ...any) {
+	if math.IsNaN(want) {
+		assert.True(t, math.IsNaN(got), msgAndArgs...)
+		return
+	}
+
+	if math.IsInf(want, +1) {
+		assert.True(t, math.IsInf(got, +1), msgAndArgs...)
+		return
+	}
+
+	if math.IsInf(want, -1) {
+		assert.True(t, math.IsInf(got, -1), msgAndArgs...)
+		return
+	}
+
+	assert.InDelta(t, want, got, delta, msgAndArgs...)
+}
 
 func Test_mergeHistogramDataPoint(t *testing.T) {
 	ts1 := time.Unix(111111111, 0)
